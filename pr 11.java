a9978b46df92ca9ed1af8dd194ed0177da600fae
bhavyaachudasama <chudasamabhavya2@gmail.com>
2026-05-01 21:31:12 +0530

    Add files via upload

---
+++ pr 11.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class MatrixChainDP {
+
+    
+    public static int matrixChainOrder(int p[], int n) {
+
+        int dp[][] = new int[n][n];
+
+        
+        for (int len = 2; len < n; len++) {
+
+            for (int i = 1; i < n - len + 1; i++) {
+
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+
+                for (int k = i; k < j; k++) {
+
+                    int cost = dp[i][k] + dp[k + 1][j]
+                            + p[i - 1] * p[k] * p[j];
+
+                    if (cost < dp[i][j]) {
+                        dp[i][j] = cost;
+                    }
+                }
+            }
+        }
+
+        return dp[1][n - 1];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of matrices: ");
+        int n = sc.nextInt();
+
+        int p[] = new int[n + 1];
+
+        System.out.println("Enter dimensions array (size " + (n + 1) + "):");
+        for (int i = 0; i <= n; i++) {
+            p[i] = sc.nextInt();
+        }
+
+        long startTime = System.nanoTime();
+
+        int result = matrixChainOrder(p, n + 1);
+
+        long endTime = System.nanoTime();
+
+        System.out.println("Minimum number of multiplications: " + result);
+        System.out.println("Execution Time (ns): " + (endTime - startTime));
+
+        sc.close();
+    }
+}
